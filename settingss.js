eeffa4aea055ffcadd432232655fbce9f0f22459
sarwarali3 <jsjdjdxjf@outlook.com>
2025-07-27 07:55:25 +0500

    Update settingss.js

--- settingss.js
+++ settingss.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "malvin~JmUHDY7D#58j3gsy1ziEacNlSGN8BQ35B2Hr5E9HDMw7TFPIh8FI",  // add your Session Id make sure it starts with malvin~
+  SESSION_ID: "malvin~Frt1nSaZ#U1RrTGY7lKhtzYNzdxIVGfptaOa-_TyekbD0fBVFVW0",  // add your Session Id make sure it starts with malvin~
   OWNER_NUMBER: "923188211549", // PUT OWNER NUMBER HERE
   PREFIX: "." // prefix (e.g., ., /, !, *)
 };
